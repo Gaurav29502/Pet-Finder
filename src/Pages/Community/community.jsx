bafa3995876d2fe0bd794d5926012a25cada1797
Gaurav29502 <gaurav.nenwani@gmail.com>
2022-07-26 11:23:23 +0530

    initial commits

---
+++ src/Pages/Community/community.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState } from "react";
+import './community.css'
+
+export const Community = (props) => {
+
+}
---
+++ src/Pages/Vets/vets.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { useState } from "react";
+import './vets.css'
+
+export const Vets = (props) => {
+    
+}
